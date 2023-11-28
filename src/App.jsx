8084688a675b493209f8398c48b85960b775c4f3
s0d4z3r0 <av.falcao@hotmail.com>
2023-11-28 10:56:55 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [shiba, setShiba] = useState([]);
+  const [change, setChange] = useState(true);
+  const [loader, setLoader] = useState(false);
+
+  useEffect(() => {
+    setLoader(true);
+
+    if (change === true) {
+      const fetchShiba = async () => {
+        await fetch(
+          `http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true`
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            setShiba(data);
+            setLoader(false);
+          })
+          .catch((err) => {
+            console.error(err);
+            setLoader(false);
+          });
+      };
+      setChange(false);
+
+      fetchShiba();
+    }
+  }, [change]);
+
+  return (
+    <div className="shibaWorld">
+      <h1>
+        Shiba <span>World</span>
+      </h1>
+      <div className="picsShiba">
+        {!loader ? (
+          shiba.map((url, index) => (
+            <div
+              className="picShiba"
+              key={index}
+              style={{ backgroundImage: `url(${url})` }}
+              onClick={() => setChange(true)}
+            ></div>
+          ))
+        ) : (
+          <div className="loader"></div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
